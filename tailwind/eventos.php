839d639371d40c2d5535df7b5bf1928e3f74ad24
psoares-iscac <psoares@iscac.pt>
2025-11-06 18:23:50 +0000

    s1

--- tailwind/eventos.php
+++ tailwind/eventos.php
@@ -10,8 +10,9 @@
 </head>
 <body>
    <?php 
-    require('includes/nav.php')
-    ?>
+   require('includes/connection.php');
+   require('includes/nav.php');
+   ?>
     
     <!-- imagem principal -->
     <div class="max-w-7xl mx-auto relative">
@@ -24,44 +25,30 @@
     <div class="mx-auto max-w-7xl">
         
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem3_min.jpg" alt="Evento 3">
-                <div class="my-1 font-semibold">29.11</div>
-                <div class="font-light mb-4">Festa de fim de semestre</div>
-                <a href="evento.php?evento=3" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
-
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem1_min.jpg" alt="Evento 3">
-                <div class="my-1 font-semibold">31.12</div>
-                <div class="font-light mb-4">Bem vindo 2026!</div>
-                <a href="evento.html?evento=1" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
-
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem2_min.jpg" alt="Evento 3">
-                <div class="my-1 font-semibold">05.04</div>
-                <div class="font-light mb-4">Páscoa Festiva</div>
-                <a href="evento.html?evento=2" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
-
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem4_min.jpg" alt="Evento 3">
-                <div class="my-1 font-semibold">fevereiro</div>
-                <div class="font-light mb-4">Festividades várias</div>
-                <a href="evento.html?evento=4" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
-            
-
+        
+        <?php
+        $sql = 'SELECT * FROM eventos';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        //echo $stmt->rowCount();
+        if($stmt && $stmt->rowCount() > 0){
+            while($evento = $stmt->fetchObject()){
+                $id     = $evento->id;
+                $nome   = $evento->nome;
+                $imagem = $evento->imagem;
+            ?>
+                <div class="">
+                    <img class="w-full h-auto" src="imgs/<?= $imagem ?>" alt="Evento <?= $id ?>">
+                    <div class="my-1 font-semibold">29.11</div>
+                    <div class="font-light mb-4"><?= $nome ?></div>
+                    <a href="evento.php?evento=<?= $id ?>" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
+                        Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
+                    </a>
+                </div>
+        <?php         
+            }// do while de cada evento
+        } // do if de haver ecventos
+        ?>
         </div>
 
     </div>
